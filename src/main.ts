ce019e77e9dc04c9bdb1b239d2fb94b3cb490994
m.dynieski <62985548+CodeTestCreate@users.noreply.github.com>
2021-07-15 15:44:16 +0200

    added base nestjs structure

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('SW Characters API')
+    .setDescription('The SW characters API')
+    .addTag('characters')
+    .setVersion('0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
